25d52a1257963120fb6c719293155729e8daa31b
shimehituzi <shimehituzi@gmail.com>
2020-02-04 20:26:38 +0900

    完成

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,15 +2,7 @@ import React, { useState } from 'react'
 import Editor from 'react-ace'
 
 export const Form: React.FC = () => {
-  const [aceValue, setAceValue] = useState<string>('')
-  const [valueArray, setValueArray] = useState<string[]>([])
-  const returnAceValueText = (lines: string[]):string => {
-    const currentline: string = lines.slice(-1)[0]
-    const beforeline: string[] = lines.slice(0, -1)
-    const resText: string = beforeline.length > 0 ?
-      beforeline.join('\n') + '\n' + currentline : currentline
-    return resText
-  }
+  const [valueArray, setValueArray] = useState<string[]>([''])
 
   const returnArray = (lines: string[], row: number, col: number):string[] => {
     const beforewords: string = valueArray.slice(row, row + 1)[0].slice(0, col)
@@ -30,17 +22,14 @@ export const Form: React.FC = () => {
   }
  
   const onAceValeuFunc = (value:string , event: any) => {
-    setAceValue(value)
     if (event.action === 'insert') {
       const lines :string[] = event.lines
       if (lines.every((str) => str.match(/^[\n\x20-\x7e]*$/) !== null)) {
-        setAceValue(value)
         setValueArray(value.split('\n'))
       } else {
         const filteredLines :string[] = lines.map((str) => { return(
           str.split('').filter((chr) => chr.match(/^[\n\x20-\x7e]$/) !== null).join('')
         )})
-        setAceValue(aceValue + returnAceValueText(filteredLines))
         const row: number = event.start.row
         const col: number = event.start.column
         setValueArray(returnArray(filteredLines, row, col))
@@ -54,9 +43,6 @@ export const Form: React.FC = () => {
   const aceValueArray = valueArray.join('\n')
 
   return (
-    <React.Fragment>
-      <Editor onChange={onAceValeuFunc} value={aceValue}/>
-      <Editor value={aceValueArray}/>
-    </React.Fragment>
+    <Editor onChange={onAceValeuFunc} value={aceValueArray}/>
   )
 }
