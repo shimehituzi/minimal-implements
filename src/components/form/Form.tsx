7852eabe6f42985c9e7610c1a1fe3b982072e7bf
shimehituzi <shimehituzi@gmail.com>
2020-02-04 17:28:00 +0900

    ちょい修正

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import Editor from 'react-ace'
 
 export const Form: React.FC = () => {
-  const asciiOnlyFlag = true
   const [aceValue, setAceValue] = useState<string>('')
   const returnAceValueText = (lines: string[]):string => {
     const currentline: string = lines.slice(-1)[0]
@@ -14,17 +13,17 @@ export const Form: React.FC = () => {
 
   const onAceValeuFunc = (value:string , event: any) => {
     setAceValue(value)
-    if (asciiOnlyFlag && event.action === 'insert') {
+    if (event.action === 'insert') {
       const lines :string[] = event.lines
       if (lines.every((str) => str.match(/^[\n\x20-\x7e]*$/) !== null)) {
-        setAceValue(aceValue + returnAceValueText(lines))
+        setAceValue(value)
       } else {
         const filteredLines :string[] = lines.map((str) => { return(
           str.split('').filter((chr) => chr.match(/^[\n\x20-\x7e]$/) !== null).join('')
         )})
         setAceValue(aceValue + returnAceValueText(filteredLines))
       }
-    } 
+    }
   }
 
   return (
