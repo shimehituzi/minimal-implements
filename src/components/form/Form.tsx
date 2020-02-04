36bb2f65f51e8aa1bd7dfc38c827cb89a0721f5b
shimehituzi <shimehituzi@gmail.com>
2020-02-04 20:40:56 +0900

    Renamed valable

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import Editor from 'react-ace'
 
 export const Form: React.FC = () => {
-  const [valueArray, setValueArray] = useState<string[]>([''])
+  const [inputSourceCode, setInputSourceCode] = useState<string[]>([''])
 
-  const returnArray = (lines: string[], row: number, col: number):string[] => {
-    const beforewords: string = valueArray.slice(row, row + 1)[0].slice(0, col)
-    const afterwords: string = valueArray.slice(row, row + 1)[0].slice(col)
-    const beforelines: string[] = valueArray.slice(0, row)
-    const afterlines: string[] = valueArray.slice(row + 1)
+  const connectAsciiSouceCode = (lines: string[], row: number, col: number):string[] => {
+    const beforewords: string = inputSourceCode.slice(row, row + 1)[0].slice(0, col)
+    const afterwords: string = inputSourceCode.slice(row, row + 1)[0].slice(col)
+    const beforelines: string[] = inputSourceCode.slice(0, row)
+    const afterlines: string[] = inputSourceCode.slice(row + 1)
     if (lines.length > 1) {
       const startwords: string = beforewords + lines.slice(0, 1)
       const endworrds: string =  lines.slice(-1) +afterwords
@@ -21,28 +21,28 @@ export const Form: React.FC = () => {
     }
   }
  
-  const onAceValeuFunc = (value:string , event: any) => {
+  const onAceValeuFunc = (fieldVal:string , event: any) => {
     if (event.action === 'insert') {
       const lines :string[] = event.lines
       if (lines.every((str) => str.match(/^[\n\x20-\x7e]*$/) !== null)) {
-        setValueArray(value.split('\n'))
+        setInputSourceCode(fieldVal.split('\n'))
       } else {
-        const filteredLines :string[] = lines.map((str) => { return(
+        const asciiLines :string[] = lines.map((str) => { return(
           str.split('').filter((chr) => chr.match(/^[\n\x20-\x7e]$/) !== null).join('')
         )})
         const row: number = event.start.row
         const col: number = event.start.column
-        setValueArray(returnArray(filteredLines, row, col))
+        setInputSourceCode(connectAsciiSouceCode(asciiLines, row, col))
       }
     }
     if (event.action === 'remove') {
-      setValueArray(value.split('\n'))
+      setInputSourceCode(fieldVal.split('\n'))
     }
   }
 
-  const aceValueArray = valueArray.join('\n')
+  const inputSourceCodeString = inputSourceCode.join('\n')
 
   return (
-    <Editor onChange={onAceValeuFunc} value={aceValueArray}/>
+    <Editor onChange={onAceValeuFunc} value={inputSourceCodeString}/>
   )
 }
