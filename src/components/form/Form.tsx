736fa03f7ff4633b674008d3910696acfa103eec
shimehituzi <shimehituzi@gmail.com>
2020-02-04 16:31:48 +0900

    テキストエリアVerを削除

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -3,27 +3,7 @@ import Editor from 'react-ace'
 
 export const Form: React.FC = () => {
   const asciiOnlyFlag = true
-  // const [value, setValue] = useState<string>('')
   const [aceValue, setAceValue] = useState<string>('')
-
-  // const onSetAnyValueFunc = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-  //   if (asciiOnlyFlag) {
-  //     const str: string = e.target.value
-  //     if (str.match(/^[\n\x20-\x7e]*$/) !== null) {
-  //       setValue(str)
-  //     } else {
-  //       setValue(
-  //         str
-  //         .split('')
-  //         .filter(char => char.match(/^[\n\x20-\x7e]$/) !== null)
-  //         .join('')
-  //       )
-  //     }
-  //   } else {
-  //     setValue(e.target.value)
-  //   }
-  // }
-
   const returnAceValueText = (lines: string[]):string => {
     const currentline: string = lines.slice(-1)[0]
     const beforeline: string[] = lines.slice(0, -1)
@@ -49,7 +29,6 @@ export const Form: React.FC = () => {
 
   return (
     <React.Fragment>
-      {/* <textarea onChange={onSetAnyValueFunc} value={value}/> */}
       <Editor onChange={onAceValeuFunc} value={aceValue} />
     </React.Fragment>
   )
