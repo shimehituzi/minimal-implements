c415fc4c41d9bd1134e48445b09009fcd2a08376
shimehituzi <shimehituzi@gmail.com>
2020-02-05 13:46:34 +0900

    Form initialized

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import CreateContainer from './containers/create/CreateContainer'
 
 const Root: React.FC = () => {
   return (
-    <React.Fragment/>
+    <CreateContainer/>
   )
 }
 
--- src/components/create/Create.tsx
+++ src/components/create/Create.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
+// import { CodeForm } from './CodeForm'
+// import { CodeCommentForm } from './CodeCommentForm'
+
+interface FormValue {
+  id: number
+  name: string
+  description: string
+  code: string[]
+  codeComment: string[]
+}
+
+const intialFromValue: FormValue = {
+  id: -1,
+  name: '',
+  description: '',
+  code: [''],
+  codeComment: [''],
+}
 
 export const Create: React.FC = () => {
+  const [formValue, setFormValue] = useState<FormValue>(intialFromValue)
+
+
+  const onKeyDownNameFunc = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') e.preventDefault()
+  }
+
+  const onChangeNameFunc = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValue({ ...formValue, name: e.target.value })
+  }
+  
+  const onChangeDescriptionFunc = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setFormValue({ ...formValue, description: e.target.value })
+  }
+
+  const onSubmitFunc = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+
+
   return (
-    <div>hihi</div>
+    <form onSubmit={onSubmitFunc}>
+      <label>
+        Name: <br/>
+        <input type="text" value={formValue.name} onKeyDown={onKeyDownNameFunc} onChange={onChangeNameFunc} size={40}/>
+      </label>
+      <br/>
+      <label>
+        Description: <br/>
+        <textarea value={formValue.description} onChange={onChangeDescriptionFunc} cols={100} rows={20}/>
+      </label>
+      <br/><br/>
+      <label>
+        <input type="submit" value={"Create"}/>
+      </label>
+    </form>
   )
 }
