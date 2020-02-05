33fc8a8e0de718be939b69adc0383013fe503eb6
shimehituzi <shimehituzi@gmail.com>
2020-02-05 18:49:09 +0900

    game

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Root from './Root'
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Link to='/'><button><h1>Home</h1></button></Link>
       <Route exact path='/' component={Root} />
+      {/* <Route path='/Game/:id' component={Game} /> */}
     </Router>
   )
 }
