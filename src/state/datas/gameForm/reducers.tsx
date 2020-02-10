16bad0b1c57e2586f353a38bff8939caad4c2cf0
shimehituzi <shimehituzi@gmail.com>
2020-02-10 12:14:42 +0900

    createGame の際のFormの処理

--- src/state/datas/gameForm/reducers.tsx
+++ src/state/datas/gameForm/reducers.tsx
@@ -1,10 +1,18 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers'
 import { actions } from './actions'
+import { actions as gameListActions } from '../gameList/actions'
 import { initialState } from './state'
 
 const reducer = reducerWithInitialState(initialState)
   .case(actions.setGameFormInput, (_, newState) => {
     return newState
+  })
+  .case(gameListActions.createNewGame, (state, _) => {
+    return {
+      ...state,
+      initialState,
+      id: state.id + 1
+    }
   })
 
 export default reducer
