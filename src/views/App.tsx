3c93ade342b3b0bc5598535bec55c888a821d8f2
shimehituzi <shimehituzi@gmail.com>
2020-02-10 13:38:46 +0900

    new file

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,14 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import HomeContainer from './containers/HomeContainer'
+
 
 const App: React.FC = () => {
   return (
-    <Fragment>
-    </Fragment>
+    <Router>
+      <Link to='/'><button><h1>Home</h1></button></Link>
+      <Route exact path='/' component={HomeContainer}/>
+    </Router>
   )
 }
 
