3a8728156eab25ef494818389e872b992e30e0ff
shimehituzi <shimehituzi@gmail.com>
2020-02-10 18:15:32 +0900

    移行完了

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import HomeContainer from './containers/HomeContainer'
+import GameContainer from './containers/GameContainer'
 
 
 const App: React.FC = () => {
@@ -8,6 +9,7 @@ const App: React.FC = () => {
     <Router>
       <Link to='/'><button><h1>Home</h1></button></Link>
       <Route exact path='/' component={HomeContainer}/>
+      <Route path='/Game/:id' component={GameContainer}/>
     </Router>
   )
 }
